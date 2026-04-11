9490005013a5f151253a27aebe6744ef8cbd468d
donnie-groot <donniegroot@gmail.com>
2026-04-11 17:17:04 +0000

    big fix: fixed file paths after file restructure

--- src/main.py
+++ src/main.py
@@ -10,34 +10,35 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
 # getting google auth working
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-###### checking if we have credintals ######
+###### checking if we have credintals ######Z
 
 
 def google_auth():
     creds = None
     # checking if credintals are already saved
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(os.path.join(base_dir, "..", "config", "token.json")):
+        creds = Credentials.from_authorized_user_file(os.path.join(base_dir, "..", "config", "token.json"), SCOPES)
     # if they arent getting u loged in
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
-            "sports_api_credentials.json", SCOPES
+            os.path.join(base_dir, "..", "config", "sports_api_credentials.json"), SCOPES
         )
         creds = flow.run_local_server(port=0)
     # opening token.json and writing the credintals to it
-    with open("token.json", "w") as file:
+    with open(os.path.join(base_dir, "..", "config", "token.json"), "w") as file:
         file.write(creds.to_json())
     # returning and building a connection to google calendar v3 api
     return build("calendar", "v3", credentials=creds)
 
 
 # load F1 schedule from local JSON file
-with open("f1_schedule_2026.json", "r", encoding="utf-8") as file:
+with open(os.path.join(base_dir, "..", "data", "f1_schedule_2026.json"), "r", encoding="utf-8") as file:
     data = json.load(file)
-
 
 ###### logic  ######
 def calendar_event(races, service):
